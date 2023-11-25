a3c4764161adeca9212df46c6899bf7658806811
Lê Nguyễn Huyền Thoại <lenguyenhuyenthoai@gmail.com>
2023-11-25 21:25:24 +0700

    add card component

---
+++ src/Components/Card/Card.tsx
@@ -0,0 +1,135 @@
+import { Colors, FontSize } from "@/Theme/Variables"
+import { View, Text, Image, StyleSheet } from "react-native"
+
+export enum CardDirection {
+    ROW = 'row',
+    COLUMN = 'column'
+}
+
+interface IProps {
+    imageUrl: string,
+    title: string,
+    subtitle: string,
+    direction?: CardDirection
+    tail?: JSX.Element
+}
+
+const Card = ({
+    imageUrl,
+    title,
+    subtitle,
+    direction,
+    tail
+}: IProps) => {
+    return (
+        <View
+            style={direction == CardDirection.ROW ?  styles.containerRow : styles.containerColumn}
+        >
+            <Image 
+                style={direction === CardDirection.ROW ? styles.imageRow : styles.imageColumn}
+                source={{
+                    uri: imageUrl
+                }}
+                resizeMode='cover'
+            />
+
+            <View 
+                style={[
+                    styles.content,
+                    direction === CardDirection.ROW && { flex: 1 }
+                ]}
+            >
+                <Text
+                    style={styles.title}
+                    numberOfLines={1}
+                    ellipsizeMode='tail'
+                >
+                    {title}
+                </Text>
+                <Text
+                    style={styles.subtitle}
+                    numberOfLines={3}
+                    ellipsizeMode='tail'
+                >
+                    {subtitle}
+                </Text>
+            </View>
+
+            <View style={styles.tail}>
+                {tail}
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    containerRow: { 
+        width: '100%',
+        elevation: 0, 
+        
+        borderRadius: 8,
+        backgroundColor: Colors.WHITE,
+        
+        shadowColor: 'rgba(0, 0, 0, 0.10)',
+        shadowOffset: { width: 0.9, height: 0.9 },
+        shadowOpacity: 1,
+        shadowRadius: 1.8,
+        
+        display: 'flex',
+        flexDirection: 'row',  
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 12
+    },
+    containerColumn: {
+        maxWidth: 220,
+        borderRadius: 8,
+        backgroundColor: Colors.WHITE,
+
+        shadowColor: 'rgba(0, 0, 0, 0.10)',
+        shadowOffset: { width: 0.9, height: 0.9 },
+        shadowOpacity: 1,
+        shadowRadius: 1.8,
+        elevation: 10,
+
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        gap: 12
+    },
+    imageColumn: {
+        width: '100%',
+        height: 200,
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8
+    },
+    imageRow: {
+        width: 120,
+        height: 120
+    },
+    content: {
+        paddingHorizontal: 16,
+        backgroundColor: Colors.WHITE,
+        
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 5,
+    },
+    title: {
+        fontWeight: '500',
+        fontSize: FontSize.SMALL,
+        color: Colors.PRIMARY_TEXT
+    },
+    subtitle: {
+        fontWeight: '400',
+        fontSize: 12,
+        color: Colors.SECONDARY_TEXT
+    },
+    tail: {
+        paddingHorizontal: 16,
+        paddingBottom: 12
+    }
+})
+
+export default Card
--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -9,6 +9,8 @@ import React from "react";
 import { View } from "react-native";
 import useInputController from "@/Components/Input/useInputController";
 import { Colors } from "@/Theme/Variables";
+import Card, { CardDirection } from "@/Components/Card/Card";
+import { faStar} from "@fortawesome/free-solid-svg-icons/faStar"
 
 export interface IHomeProps {
   data: User | undefined;
@@ -34,6 +36,23 @@ export const Home = (props: IHomeProps) => {
             color={passwordController.isFocused ? Colors.PRIMARY : Colors.BACKGROUND} 
           />
         }
+      />
+
+      <Card 
+        imageUrl="https://images.unsplash.com/photo-1602253057119-44d745d9b860?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D"
+        title='Tên món ăn ở đây'
+        subtitle="Mô tả món ăn ở đây nha, có handle overflow text òi nên không cần lo nữa ạ"
+        direction={CardDirection.COLUMN}
+        tail={
+          <FontAwesomeIcon icon={faStar} color={Colors.ERROR} />
+        }
+      />
+
+      <Card 
+        imageUrl="https://images.unsplash.com/photo-1602253057119-44d745d9b860?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D"
+        title='Tên món ăn ở đây'
+        subtitle="Mô tả món ăn ở đây nha, có handle overflow text òi nên không cần lo nữa ạ"
+        direction={CardDirection.ROW}
       />
     </View>
   );
